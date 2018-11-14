5cec63e933c96b69a6e3601ceb45418823820577
kkamara <itchykilljoy@protonmail.com>
2018-11-14 19:22:17 +0000

    added square_every_digit

---
+++ square_every_digit/test_square_every_digit.py
@@ -0,0 +1,9 @@
+import unittest
+from main import square_digits
+
+class TestSquareEveryDigit(unittest.TestCase):
+    def test_return_value(self):
+        self.assertEqual(811181, square_digits(9119))
+
+if __name__ == '__main__':
+    unittest.main()
