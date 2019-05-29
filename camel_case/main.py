f4a6e74415df6a8de3dfa82194372d47e5c1997c
kkamara <itchykilljoy@protonmail.com>
2019-05-29 20:12:24 +0100

    added camel case removed __pycache__ folders

---
+++ camel_case/main.py
@@ -0,0 +1,13 @@
+
+# All words must have their first letter capitalized without spaces.
+# For instance:
+# camelcase("hello case") => HelloCase
+# camelcase("camel case word") => CamelCaseWord
+
+def execute(string):
+    col = string.split(' ')
+    r = ''
+    for word in col:
+        r += word.title()
+    return r
+
